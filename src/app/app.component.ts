71c62012983b10a346aa77ecc232c8e2e58e9487
UST User <ustuser@A032MLTRV.local>
2019-02-11 13:07:34 +0530

    src commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,47 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AlertController } from 'ionic-angular';
 import { HomePage } from '../pages/home/home';
+
+declare var SMS: any;
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private alertCtrl: AlertController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      if (SMS) SMS.startWatch(() => {
+        console.log('watching started');
+        this.presentAlert("Alert",'watching started');
+      }, Error => {
+        console.log('failed to start watching');
+        this.presentAlert("Alert",'failed to start watching');
+      });
+
+      document.addEventListener('onSMSArrive', (e: any) => {
+        var sms = e.data;
+        console.log(sms);
+        this.presentAlert("SMS recieved ", "From ->"+sms.address+" Message is ->"+sms.body);
+      });
     });
+  }
+
+  presentAlert(title, msg) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: msg,
+      buttons: ['Dismiss']
+    });
+    alert.present();
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { AlertController } from 'ionic-angular';
+
+declare var SMS: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,65 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public androidPermissions: AndroidPermissions, public platform: Platform, private alertCtrl: AlertController) {
 
   }
 
+  ionViewWillEnter() {
+
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.READ_SMS).then(
+      success => console.log('Permission granted'),
+      err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.READ_SMS)
+    );
+
+    this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.READ_SMS]);
+
+    this.platform.ready().then((readySource) => {
+
+      // if (SMS) SMS.startWatch(() => {
+      //   console.log('watching started');
+      //   this.presentAlert("Alert",'watching started');
+      // }, Error => {
+      //   console.log('failed to start watching');
+      //   this.presentAlert("Alert",'failed to start watching');
+      // });
+
+      // document.addEventListener('onSMSArrive', (e: any) => {
+      //   var sms = e.data;
+      //   console.log(sms);
+      //   this.presentAlert("SMS recieved ", "From ->"+sms.address+" Message is ->"+sms.body);
+      // });
+
+    });
+  }
+
+  SendMySMS() {
+
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.SEND_SMS).then(
+      success => console.log('Permission granted'),
+      err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.SEND_SMS)
+    );
+
+    this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.SEND_SMS]);
+
+    this.platform.ready().then((readySource) => {
+
+      if (SMS) SMS.sendSMS("+919884950017", "SMS from App to test", () => {
+        console.log("Sent");
+        this.presentAlert("Alert",'SMS Sent');
+      }, Error => {
+        console.log("Error occurs")
+        this.presentAlert("Alert",'SMS not sent '+Error);
+      });
+    });
+  }
+
+  presentAlert(title, msg) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: msg,
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
 }
